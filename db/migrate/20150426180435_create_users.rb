79a4b8f6949eb69e26edfb55b3e5b7eaba3ebf82
Arkadiusz Kusy <kusyarkadiusz@gmail.com>
2015-04-27 00:32:15 +0100

    Update users migration

--- db/migrate/20150426180435_create_users.rb
+++ db/migrate/20150426180435_create_users.rb
@@ -2,9 +2,9 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name, null: false
-      t.string :email, null: false
+      t.string :email
       t.string :role, null: false
-      t.string :password_digest, null: false
+      t.string :password_digest
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20150426180435) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            null: false
-    t.string   "email",           null: false
+    t.string   "email"
     t.string   "role",            null: false
-    t.string   "password_digest", null: false
+    t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
