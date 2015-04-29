3a3241a195a8ae193fead1b52183efc5e281a9c5
Arkadiusz Kusy <kusyarkadiusz@gmail.com>
2015-04-30 00:07:56 +0100

    Add basic http authentication and setting guest user

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -2,6 +2,27 @@ module Api
   class ApplicationController < ::ApplicationController
     skip_before_action  :verify_authenticity_token
 
+    before_action :authenticate_or_set_guest
+
     respond_to :json
+
+    def current_user
+      @current_user
+    end
+
+    def authenticate_or_set_guest
+      authenticate_or_request_with_http_basic("Application") do |email, password|
+        return set_guest_user unless email.present?
+        if user = User.find_by(email: email)
+          @current_user = user.authenticate(password)
+        end
+      end
+    end
+
+    private
+
+    def set_guest_user
+      @current_user = User.create_guest
+    end
   end
 end
