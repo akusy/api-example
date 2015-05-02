eec65052bbff3884e2b2d00534d12453ca3795ea
Arkadiusz Kusy <kusyarkadiusz@gmail.com>
2015-05-02 18:34:35 +0100

    Update config/routes with articles API

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:show, :update, :delete, :index]
+    resources :users, only: [:show, :update, :destroy, :index, :create] do
+      resources :articles, only: [:show, :update, :destroy, :index, :create]
+    end
   end
 
 end
