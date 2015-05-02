43762bae4ee827afc87f5a2bf6caa427cf1ea2aa
Arkadiusz Kusy <kusyarkadiusz@gmail.com>
2015-05-02 18:36:28 +0100

    Update spec authentication helper

--- spec/http_authentication_helper.rb
+++ spec/http_authentication_helper.rb
@@ -1,5 +1,5 @@
 module HttpAuthenticationHelper
-  def http_authenticate_user email, password
+  def http_authenticate_or_request email, password
     request.env['HTTP_AUTHORIZATION'] =
       ActionController::HttpAuthentication::Basic.encode_credentials(email, password)
   end
