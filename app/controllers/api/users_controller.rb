027341be5b968521d77be08810d223826985a5d5
Arkadiusz Kusy <kusyarkadiusz@gmail.com>
2015-05-02 15:38:28 +0100

    Add rest of CRUD methods to user API controller and update specs

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,8 +1,36 @@
 module Api
   class UsersController < Api::ApplicationController
 
+    before_action :http_authenticate_or_request
+
     def show
-      respond_with User.find(params[:id])
+      respond_with user
+    end
+
+    def index
+      respond_with User.page(page)
+    end
+
+    def update
+      if user.update(user_params)
+        render json: user
+      else
+        render json: { errors: user.errors.messages }, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      respond_with user.destroy
+    end
+
+    private
+
+    def user
+      User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :name, :password)
     end
   end
 end
--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -9,13 +9,64 @@ describe Api::UsersController do
 
       it { is_expected.to respond_with(:unauthorized) }
     end
+
+    describe "#index" do
+      before do
+        get :index, format: :json
+      end
+
+      it { is_expected.to respond_with(:unauthorized) }
+    end
+
+    describe "#destroy" do
+      before do
+        get :destroy, id: user.id, format: :json
+      end
+
+      it { is_expected.to respond_with(:unauthorized) }
+    end
+
+    describe "#update" do
+      before do
+        get :update, id: user.id, format: :json
+      end
+
+      it { is_expected.to respond_with(:unauthorized) }
+    end
   end
 
   context "With basic authentication" do
+    before do
+      http_authenticate_or_request user.email, user.password
+    end
+
     describe "#show" do
       before do
-        http_authenticate_user user.email, user.password
         get :show, id: user.id, format: :json
+      end
+
+      it { is_expected.to respond_with(:ok) }
+    end
+
+    describe "#update" do
+      before do
+        get :update, id: user.id, user: { name: "test" }, format: :json
+      end
+
+      it { is_expected.to respond_with(:ok) }
+    end
+
+    describe "#index" do
+      before do
+        get :index, format: :json
+      end
+
+      it { is_expected.to respond_with(:ok) }
+    end
+
+    describe "#destroy" do
+      before do
+        get :destroy, id: user.id, format: :json
       end
 
       it { is_expected.to respond_with(:ok) }
