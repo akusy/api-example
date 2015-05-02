be34e157a92e72925901b2851ea6217b329ce29b
Arkadiusz Kusy <kusyarkadiusz@gmail.com>
2015-05-02 23:12:22 +0100

    Add user basic authentication helper

--- spec/http_authentication_helper.rb
+++ spec/http_authentication_helper.rb
@@ -3,4 +3,10 @@ module HttpAuthenticationHelper
     request.env['HTTP_AUTHORIZATION'] =
       ActionController::HttpAuthentication::Basic.encode_credentials(email, password)
   end
+
+  def basic_http_header email, password
+    {
+      'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(email, password)
+    }
+  end
 end
