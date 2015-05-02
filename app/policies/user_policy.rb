dc15331759a7e9b806787f312ffa4f14fddadf24
Arkadiusz Kusy <kusyarkadiusz@gmail.com>
2015-05-02 22:06:39 +0100

    Create user policy

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,20 @@
+class UserPolicy < ApplicationPolicy
+
+  [:show?, :update?, :destroy?].each do |method|
+    define_method(method) { record_owner_or_admin? }
+  end
+
+  [:create?, :index?].each do |method|
+    define_method(method) { record_admin? }
+  end
+
+  private
+
+  def record_owner_or_admin?
+    user == record || record_admin?
+  end
+
+  def record_admin?
+    user.admin?
+  end
+end
