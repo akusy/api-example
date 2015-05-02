6e5efc73736344a20112b19a902ee6d74da55832
Arkadiusz Kusy <kusyarkadiusz@gmail.com>
2015-05-02 15:38:53 +0100

    Improve users validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ActiveRecord::Base
 
   validates :email, uniqueness: true, allow_nil: true
   validates :email, presence: true, email: true, unless: :guest?
-  validates :password, presence: true, length: { in: 6..30 }, unless: :guest?
-  validates :name, :role, presence: true
+  validates :password, length: { in: 6..30 }, on: :create, unless: :guest?
+  validates :password, presence: true, on: :create, unless: :guest?
+  validates :name, presence: true
   validates :role, presence: true, inclusion: { in: ROLES,
     message: "%{value} is not a valid role" }
 
